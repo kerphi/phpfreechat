6c468ce8d18c5a7d4b9c9ee4c65820a97e3eecb0
kerphi <stephane.gully@gmail.com>
2006-01-09 14:47:38 +0000

    correct the wrong path
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/trunk@35 2772adf2-ac07-0410-9d30-e29d8120292e

--- src/phpxchat_container_file.class.php
+++ src/phpxchat_container_file.class.php
@@ -11,7 +11,7 @@ class phpXChat_Container_File extends phpXChat_Container
     $c =& $this->c;
     
     $cfg = array();
-    $cfg["chat_dir"]            = $c->data_private."".md5($c->title)."/";
+    $cfg["chat_dir"]            = $c->data_private."/".md5($c->title)."/";
     $cfg["data_file"]           = $cfg["chat_dir"]."messages.data";
     $cfg["index_file"]          = $cfg["chat_dir"]."messages.index";
     $cfg["nickname_dir"]        = $cfg["chat_dir"]."nicknames/";
