ceec30939a500f1bb2d9d19f520f16b2bea90d6f
kerphi <stephane.gully@gmail.com>
2006-01-15 16:04:38 +0000

    fix warning
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/trunk@66 2772adf2-ac07-0410-9d30-e29d8120292e

--- debug/log.php
+++ debug/log.php
@@ -5,7 +5,7 @@ function pxlog($data, $section = "", $id = "")
   $msg = htmlspecialchars(stripslashes(var_export($data, true)));
   $filename = dirname(__FILE__)."/../data/private/debug".$section."_".$id.".log";
   if (!file_exists($filename))
-    touch($filename);
+    @touch($filename);
   $fp = fopen($filename, 'a');
   fwrite($fp, date("Y/m/d H:i:s - ").$msg."\n");
   fclose($fp);
