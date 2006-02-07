d91a97125a1eb5e05baa004536f00b14183583ab
kerphi <stephane.gully@gmail.com>
2006-02-07 10:13:47 +0000

    add a quick demo in the index
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/trunk@157 2772adf2-ac07-0410-9d30-e29d8120292e

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
+<?php
+
+require_once dirname(__FILE__)."/src/phpfreechat.class.php";
+$params = array();
+$params["nick"] = "guest".rand(1,10);  // setup the intitial nickname
+$chat = new phpFreeChat( $params );
+
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 
   <head>
     <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
-    <title>phpFreeChat demod</title>
+    <title>phpFreeChat sources index</title>
+    <?php $chat->printJavascript(); ?>
+    <?php $chat->printStyle(); ?>
   </head>
     
   <body>
@@ -14,6 +24,10 @@
       <li><a href="INSTALL.en">Documentation - install [en]</a></li>
       <li><a href="INSTALL.fr">Documentation - install [fr]</a></li>
     </ul>
+
+  <p>See the quick demo :</p>
+  <?php $chat->printChat(); ?>
+
   </body>
   
 </html>
