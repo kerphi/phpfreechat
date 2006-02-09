e9f9670c6d2371cadfeef8e02f5ef3554c26ef72
kerphi <stephane.gully@gmail.com>
2006-02-09 10:41:15 +0000

    change the configuration option name
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/trunk@171 2772adf2-ac07-0410-9d30-e29d8120292e

--- demo/demo3_config.php
+++ demo/demo3_config.php
@@ -3,6 +3,6 @@
 require_once dirname(__FILE__)."/../src/phpfreechatconfig.class.php";
 $params = array("title" => "A chat with one script for client and on script for server",
                 "nick" => "guest",
-                "server_file" => "demo3_server.php" );
+                "server_script" => "demo3_server.php" );
 
 ?>
