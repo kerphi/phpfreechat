589ec831cc6ed5cce831552827894fc03860c1f3
kerphi <stephane.gully@gmail.com>
2006-02-15 08:51:59 +0000

    typo
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/trunk@219 2772adf2-ac07-0410-9d30-e29d8120292e

--- demo/index.php
+++ demo/index.php
@@ -13,7 +13,7 @@
       <li><a href="demo3_client.php">demo3 - demo with separate client/server files</a></li>
       <li><a href="demo4_simulate_slow_server.php">demo4 - demo which simulate slow server responses</a></li>
       <li><a href="demo5_customized_style.php">demo5 - demo with a customized style</a></li>
-      <li><a href="demo6_en_francais.php">demo6 - french translation of the chat</a></li>
+      <li><a href="demo6_in_french.php">demo6 - french translation of the chat</a></li>
       <li><a href="demo7_in_english.php">demo7 - english translation of the chat</a></li>
       <li><a href="demo8_with_a_iso-8859-1_encoded_page.php">demo8 - a chat with a ISO-8859-1 encoded page</a></li>
       <li><a href="demo9_with_a_utf8_encoded_nickname.php">demo9 - a chat with a UTF-8 encoded nickname (none latin)</a></li>
