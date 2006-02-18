c6d5e10cf7830a1fe2324604632db66bf8f3573c
kerphi <stephane.gully@gmail.com>
2006-02-18 20:15:21 +0000

    New japanese (ja_JP) translation (many thanks to elf2000)
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/trunk@234 2772adf2-ac07-0410-9d30-e29d8120292e

---
+++ i18n/ja_JP/main.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * i18n/ja_JP/main.php
+ *
+ * Copyright © 2006 Stephane Gully <stephane.gully@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details. 
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+/**
+ * Japanese translation of the messages (utf8 encoded!)
+ *
+ * @author Tadashi Jokagi <elf2000@users.sourceforge.net>
+ */
+
+// line 45 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["My Chat"] = "私のチャット";
+
+// line 201 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s not found, %s library can't be found."] = "%s が見つかりません。ライブラリ %s を見つけることができません。";
+
+// line 355 in phpfreechat.class.php
+$GLOBALS["i18n"]["Please enter your nickname"] = "ニックネームを入力してください。";
+
+// line 565 in phpfreechat.class.php
+$GLOBALS["i18n"]["Text cannot be empty"] = "テキストは空にできません。";
+
+// line 392 in phpfreechat.class.php
+$GLOBALS["i18n"]["%s changes his nickname to %s"] = "%s さんはニックネームを %s に変更しました。";
+
+// line 398 in phpfreechat.class.php
+$GLOBALS["i18n"]["%s is connected"] = "%s さんが接続しました。";
+
+// line 452 in phpfreechat.class.php
+$GLOBALS["i18n"]["%s quit"] = "%s さんが終了しました。";
+
+// line 468 in phpfreechat.class.php
+$GLOBALS["i18n"]["%s disconnected (timeout)"] = "%s さんは切断しました(タイムアウト)";
+
+// line 262 in phpfreechat.class.php
+$GLOBALS["i18n"]["Unknown command [%s]"] = "不明のコマンド [%s]";
+
+// line 149 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s doesn't exist: %s"] = "%s が存在しません: %s";
+
+// line 180 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["You need %s"] = "%s が必要です。";
+
+// line 241 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s doesn't exist, %s library can't be found"] = "%s が存在しません。ライブラリ %s を見つけることができません。";
+
+// line 280 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s doesn't exist"] = "%s が存在しません。";
+
+// line 433 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s directory must be specified"] = "ディレクトリ %s を指定しなければなりません。";
+
+// line 439 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s must be a directory"] = "%s はディレクトリでなければいけません。";
+
+// line 446 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s can't be created"] = "%s を作成できませんでした。";
+
+// line 451 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s is not writeable"] = "%s を書き込みできません。";
+
+// line 496 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s is not readable"] = "%s を読み込みできません。";
+
+// line 469 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s is not a file"] = "%s はファイルではありません。";
+
+// line 491 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s is not a directory"] = "%s はディレクトリではありません。";
+
+// line 23 in chat.html.tpl.php
+$GLOBALS["i18n"]["PHP FREE CHAT [powered by phpFreeChat-%s]"] = "PHP フリーチャット [powered by phpFreeChat-%s]";
+
+// line 296 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Hide nickname marker"] = "ニックネームの色を隠す";
+
+// line 304 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Show nickname marker"] = "ニックネームの色を表示する";
+
+// line 389 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Disconnect"] = "切断する";
+
+// line 395 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Connect"] = "接続する";
+
+// line 427 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Magnify"] = "拡大する";
+
+// line 434 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Cut down"] = "カットダウン";
+
+// line 345 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Hide dates and hours"] = "日付と時間を隠す";
+
+// line 353 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Show dates and hours"] = "日付と時間を表示する";
+
+// line 21 in chat.html.tpl.php
+$GLOBALS["i18n"]["Enter your message here"] = "ここにメッセージを入力します。";
+
+// line 24 in chat.html.tpl.php
+$GLOBALS["i18n"]["Enter your nickname here"] = "ここにニックネームを入力します。";
+
+// line 93 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["Error: undefined or obsolete parameter '%s', please correct or remove this parameter"] = "エラー: パラメーター「%s」は破棄されたか未定義です。このパラメーターを修正するか削除してください。";
+
+?>
