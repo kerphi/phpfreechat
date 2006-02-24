8beaf362b0bdc31c14c797570825bc7ee0497de0
Nemako <nemako@gmail.com>
2006-02-24 10:42:27 +0000

    + Add themes/blune, demo/demo28_blune_theme.php and demo/demo28_mini_blune_theme.php
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/trunk@289 2772adf2-ac07-0410-9d30-e29d8120292e

---
+++ themes/blune/templates/style.css.tpl.php
@@ -0,0 +1,30 @@
+
+div#<?php echo $prefix; ?>container {
+  color: #2A4064;
+  background-color: #BEC5D0;
+  background-image: url(<?php echo $c->getFileUrlFromTheme('images/shade.gif'); ?>);
+}
+
+div.<?php echo $prefix; ?>message {
+  background-color:#CED4DF;
+}
+
+div.<?php echo $prefix; ?>oldmsg {
+  background-color:#DCDEE4;
+}
+
+span.<?php echo $prefix; ?>nick {
+color:#2A4064;
+}
+
+div.<?php echo $prefix; ?>btn img:hover {
+  border: 1px solid #000;
+}
+
+div#<?php echo $prefix; ?>online {
+    height: 48%;
+}
+
+div#<?php echo $prefix; ?>smileys {
+    height: 48%;
+}
