135e63e080e7eed3bab2fc97f27fcd99f74e9524
kerphi <stephane.gully@gmail.com>
2006-03-02 08:59:47 +0000

    integrate the new portuguese from Portugal translation (thanks to Paulo Bras)
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/trunk@329 2772adf2-ac07-0410-9d30-e29d8120292e

--- demo/index.php
+++ demo/index.php
@@ -7,41 +7,56 @@
   </head>
     
   <body>
-    <ul>
+<h2>Miscellaneous</h2>
+
+<ul>
       <li><a href="demo1_simple.php">demo1 - demo with default parameters</a></li>
       <li><a href="demo2_simple_with_params.php">demo2 - demo with few parameters</a></li>
       <li><a href="demo3_client.php">demo3 - demo with separate client/server files</a></li>
       <li><a href="demo4_simulate_slow_server.php">demo4 - demo which simulate slow server responses</a></li>
-      <li><a href="demo5_customized_style.php">demo5 - demo with a customized style</a></li>
-      <li><a href="demo6_in_french.php">demo6 - french translation of the chat</a></li>
-      <li><a href="demo7_in_english.php">demo7 - english translation of the chat</a></li>
+      <li><a href="demo5_customized_style.php">demo5 - demo with a customized style (theme)</a></li>
       <li><a href="demo8_with_a_iso-8859-1_encoded_page.php">demo8 - a chat with a ISO-8859-1 encoded page</a></li>
       <li><a href="demo9_with_a_utf8_encoded_nickname.php">demo9 - a chat with a UTF-8 encoded nickname (none latin)</a></li>
-      <li><a href="demo10_in_chinese.php">demo10 - the chinese translation of the chat</a></li>
+      <li><a href="demo15_multiple_channel.php">demo15 - a chat with multiple/dynamic channels</a></li>
+      <li><a href="demo21_with_hardcoded_urls.php">demo21 - with hardcoded url: data_public_url, client_script_url ... (interesting for strange server configuration)</a></li>
+      <li><a href="demo27_customized_command.php">demo27 - a customized command (/roll)</a></li>
+      <li><a href="demo31_show_who_is_online-chat.php">demo31 - demo which show how to get the connected users list (chat script)</a></li>
+      <li><a href="demo31_show_who_is_online-whoisonline.php">demo31 - demo which show how to get the connected users list (whoisonline script)</a></li>
+      <li><a href="demo32_show_last_messages-chat.php">demo32 - demo which show how to get the last posted messages (chat script)</a></li>
+      <li><a href="demo32_show_last_messages-showlastmsg.php">demo32 - demo which show how to get the last posted messages (showlastmsg script)</a></li>
+</ul>
+
+
+<h2>Themes</h2>
+
+<ul>
       <li><a href="demo11_famfamfam_smiley_theme.php">demo11 - A chat with a customized smiley theme (famfamfam theme)</a></li>
       <li><a href="demo12_phoenity_smiley_theme.php">demo12 - A chat with a customized smiley theme (phoenity theme)</a></li>
-      <li><a href="demo13_in_german_informal_language.php">demo13 - the german (informal) translation of the chat</a></li>
-      <li><a href="demo14_in_german_formal_language.php">demo14 - the german (formal) translation of the chat</a></li>
-      <li><a href="demo15_multiple_channel.php">demo15 - a chat with multiple/dynamic channels</a></li>
-      <li><a href="demo16_in_arabic.php">demo16 - the arabic translation of the chat</a></li>
       <li><a href="demo17_cerutti_smiley_theme.php">demo17 - A chat with a customized smiley theme (cerutti theme)</a></li>
       <li><a href="demo18_phpbb2_smiley_theme.php">demo18 - A chat with a customized smiley theme (phpbb2 theme)</a></li>
+      <li><a href="demo28_blune_theme.php">demo28 - use a customized theme (Blune)</a></li>
+      <li><a href="demo28_mini_blune_theme.php">demo28 - use a customized theme (Blune) - mini</a></li>
+</ul>
+
+<h2>Translations</h2>
+    <ul>
+      <li><a href="demo6_in_french.php">demo6 - french translation of the chat</a></li>
+      <li><a href="demo7_in_english.php">demo7 - english translation of the chat</a></li>
+      <li><a href="demo10_in_chinese.php">demo10 - the chinese translation of the chat</a></li>
+      <li><a href="demo13_in_german_informal_language.php">demo13 - the german (informal) translation of the chat</a></li>
+      <li><a href="demo14_in_german_formal_language.php">demo14 - the german (formal) translation of the chat</a></li>
+      <li><a href="demo16_in_arabic.php">demo16 - the arabic translation of the chat</a></li>
       <li><a href="demo19_in_japanese.php">demo19 - the japanese translation of the chat</a></li>
       <li><a href="demo20_in_brazilian_portuguese.php">demo20 - the brazilian portuguese translation of the chat</a></li>
-      <li><a href="demo21_with_hardcoded_urls.php">demo21 - with hardcoded url: data_public_url, client_script_url ... (interesting for strange server configuration)</a></li>
       <li><a href="demo22_in_serbian_croatian.php">demo22 - the serbian croatian translation of the chat</a></li>
       <li><a href="demo23_in_italian.php">demo23 - the italian translation of the chat</a></li>
       <li><a href="demo24_in_spanish.php">demo24 - the spanish translation of the chat</a></li>
       <li><a href="demo25_in_indonesian.php">demo25 - the indonesian translation of the chat</a></li>
       <li><a href="demo26_in_swedish.php">demo26 - the swedish translation of the chat</a></li>
-      <li><a href="demo27_customized_command.php">demo27 - a customized command (/roll)</a></li>
-      <li><a href="demo28_blune_theme.php">demo28 - use a customized theme (Blune)</a></li>
-      <li><a href="demo28_mini_blune_theme.php">demo28 - use a customized theme (Blune) - mini</a></li>
       <li><a href="demo29_in_russian.php">demo29 - the russian translation of the chat</a></li>
-      <li><a href="demo31_show_who_is_online-chat.php">demo31 - demo which show how to get the connected users list (chat script)</a></li>
-      <li><a href="demo31_show_who_is_online-whoisonline.php">demo31 - demo which show how to get the connected users list (whoisonline script)</a></li>
-      <li><a href="demo32_show_last_messages-chat.php">demo32 - demo which show how to get the last posted messages (chat script)</a></li>
-      <li><a href="demo32_show_last_messages-showlastmsg.php">demo32 - demo which show how to get the last posted messages (showlastmsg script)</a></li>
+      <li><a href="demo33_in_portuguese_from_portugal.php">demo33 - the Portuguese from Portugal translation of the chat</a></li>
+
+
     </ul>
   </body>
   
---
+++ i18n/pt_PT/main.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * i18n/pt_PT/main.php
+ *
+ * Copyright © 2006 Stephane Gully <stephane.gully@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details. 
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+/**
+ * Portuguese version from Portugal translation of the messages (utf8 encoded!)
+ *
+ * @author Stephane Gully <stephane.gully@gmail.com>
+ * @translation Paulo Bras
+ */
+
+// line 45 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["My Chat"] = "Chat";
+
+// line 201 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s not found, %s library can't be found."] = "%s nao encontrado, %s biblioteca não encontrada.";
+
+// line 355 in phpfreechat.class.php
+$GLOBALS["i18n"]["Please enter your nickname"] = "Por favor, indique o seu nome de utilizador";
+
+// line 565 in phpfreechat.class.php
+$GLOBALS["i18n"]["Text cannot be empty"] = "Deverá inserir algo na linha de texto";
+
+// line 392 in phpfreechat.class.php
+$GLOBALS["i18n"]["%s changes his nickname to %s"] = "%s modifica o nome de utilizador para %s";
+
+// line 398 in phpfreechat.class.php
+$GLOBALS["i18n"]["%s is connected"] = "%s está ligado";
+
+// line 452 in phpfreechat.class.php
+$GLOBALS["i18n"]["%s quit"] = "%s sair";
+
+// line 468 in phpfreechat.class.php
+$GLOBALS["i18n"]["%s disconnected (timeout)"] = "%s desligado (excesso de tempo)";
+
+// line 262 in phpfreechat.class.php
+$GLOBALS["i18n"]["Unknown command [%s]"] = "comando desconhecido [%s]";
+
+// line 149 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s doesn't exist: %s"] = "%s não existe: %s";
+
+// line 180 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["You need %s"] = "Necessita %s";
+
+// line 241 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s doesn't exist, %s library can't be found"] = "%s não existe, %s biblioteca não encontrada";
+
+// line 280 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s doesn't exist"] = "%s não existe";
+
+// line 433 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s directory must be specified"] = "%s deve especificar a directoria";
+
+// line 439 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s must be a directory"] = "%s deve ser uma directoria";
+
+// line 446 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s can't be created"] = "%s não pode ser criado";
+
+// line 451 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s is not writeable"] = "%s não se pode escrever";
+
+// line 496 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s is not readable"] = "%s não se pode lêr";
+
+// line 469 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s is not a file"] = "%s não é um ficheiro";
+
+// line 491 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s is not a directory"] = "%s não é uma directoria";
+
+// line 23 in chat.html.tpl.php
+$GLOBALS["i18n"]["PHP FREE CHAT [powered by phpFreeChat-%s]"] = "PHP FREE CHAT [powered by phpFreeChat-%s]";
+
+// line 296 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Hide nickname marker"] = "Escnder côres do nome de utilizador";
+
+// line 304 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Show nickname marker"] = "Mostrar as côres do nome de utilizador";
+
+// line 389 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Disconnect"] = "Desligar";
+
+// line 395 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Connect"] = "Ligar";
+
+// line 427 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Magnify"] = "Ampliar";
+
+// line 434 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Cut down"] = "Cortar";
+
+// line 345 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Hide dates and hours"] = "Esconder datas e horas";
+
+// line 353 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Show dates and hours"] = "Mostrar datas e horas";
+
+// line 21 in chat.html.tpl.php
+$GLOBALS["i18n"]["Enter your message here"] = "Insira a mensagem aqui";
+
+// line 24 in chat.html.tpl.php
+$GLOBALS["i18n"]["Enter your nickname here"] = "Insira o seu nome de utilizador aqui";
+
+?>
