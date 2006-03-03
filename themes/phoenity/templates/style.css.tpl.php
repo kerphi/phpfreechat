e936d99167ec1046b6f83f56d685e6915daeb913
kerphi <stephane.gully@gmail.com>
2006-03-03 19:09:47 +0000

    resize the smileys box
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/trunk@344 2772adf2-ac07-0410-9d30-e29d8120292e

---
+++ themes/phoenity/templates/style.css.tpl.php
@@ -0,0 +1,12 @@
+div#<?php echo $prefix; ?>chat {
+  width: 89%;
+}
+
+div#<?php echo $prefix; ?>smileys {
+  width: 10%;
+  height: 20%;
+}
+div#<?php echo $prefix; ?>online {
+  width: 10%;
+  height: 77%;
+}
---
+++ themes/phpbb2/templates/style.css.tpl.php
@@ -0,0 +1,12 @@
+div#<?php echo $prefix; ?>chat {
+  width: 84%;
+}
+
+div#<?php echo $prefix; ?>smileys {
+  width: 15%;
+  height: 25%;
+}
+div#<?php echo $prefix; ?>online {
+  width: 15%;
+  height: 72%;
+}
