5ba1b7a50089b5ae95e722edc9e488f5a2ca5501
kerphi <stephane.gully@gmail.com>
2006-03-28 21:28:08 +0000

    Oups the IE6 display workeround was misplaced. It must not be placed in the page header.
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/trunk@395 2772adf2-ac07-0410-9d30-e29d8120292e

--- src/phpfreechat.class.php
+++ src/phpfreechat.class.php
@@ -79,17 +79,6 @@ class phpFreeChat
     $output = '';
     $c =& phpFreeChatConfig::Instance();
     phpFreeChatI18N::SwitchOutputEncoding($c->output_encoding);
-
-    // this is a IE6 workeround (IE7 works well) to resize correctly the smiley and online boxes
-    // this is ugly but I didn't found a cleaner way to fix the problem...
-    $output .= '<!--[if lt IE 7]>';
-    $output .= '  <script type="text/javascript">';
-    $output .= '    var src = "http://www.phpfreechat.net/blank.js?";';
-    $output .= '    for (var i=0; i < 46 ; i++)';
-    $output .= '      src = src + "0000000000111111111122222222223333333333444444444455555555556666666666777777777788888888889999999999";';
-    $output .= '    document.writeln(\'<img src="\'+src+\'" alt="" style="display: none;" />\');';
-    $output .= '</script>';
-    $output .= '<![endif]-->';
     
     // include javascript libraries
     $js_path = $c->data_public_url."/javascript";
@@ -105,7 +94,6 @@ class phpFreeChat
     $output .= "<script type=\"text/javascript\">\n<!--\n";
     $output .= $t->getOutput();
     $output .= "\n-->\n</script>\n";
-
     
     // print xajax javascript
     $output .= $this->xajax->getJavascript($c->data_public_url, NULL, $c->data_public_url."/xajax_js/xajax.js");
