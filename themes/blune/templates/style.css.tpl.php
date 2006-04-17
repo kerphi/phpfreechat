a2a11e046a208a4cf652d1024f00364465ff1373
kerphi <stephane.gully@gmail.com>
2006-04-17 14:57:18 +0000

    correct the background color for the blune theme
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/branches/0.x@441 2772adf2-ac07-0410-9d30-e29d8120292e

--- themes/blune/templates/style.css.tpl.php
+++ themes/blune/templates/style.css.tpl.php
@@ -5,6 +5,10 @@ div#<?php echo $prefix; ?>container {
   background-image: url(<?php echo $c->getFileUrlFromTheme('images/shade.gif'); ?>);
 }
 
+div#<?php echo $prefix; ?>chat {
+  background-color:#CED4DF;
+}
+
 div.<?php echo $prefix; ?>message {
   background-color:#CED4DF;
 }
@@ -14,7 +18,7 @@ div.<?php echo $prefix; ?>oldmsg {
 }
 
 span.<?php echo $prefix; ?>nick {
-color:#2A4064;
+  color:#2A4064;
 }
 
 div.<?php echo $prefix; ?>btn img:hover {
@@ -22,11 +26,11 @@ div.<?php echo $prefix; ?>btn img:hover {
 }
 
 div#<?php echo $prefix; ?>online {
-    height: 48%;
+  height: 48%;
 }
 
 div#<?php echo $prefix; ?>smileys {
-    height: 48%;
+  height: 48%;
 }
 
 p#<?php echo $prefix; ?>errors {
