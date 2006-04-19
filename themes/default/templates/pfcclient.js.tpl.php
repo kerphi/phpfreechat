aeb0f5ad65e5f2078fd7c40a1d320cb0ecff534f
kerphi <stephane.gully@gmail.com>
2006-04-19 19:58:43 +0000

    Bug fix: IE doesn't support white-space:pre CSS rule, a workaround is to use <nobr> element. This is not XHTML complient but it's just a workeround for this crappy IE... (thanks to Rhialto for the tip)
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/trunk@458 2772adf2-ac07-0410-9d30-e29d8120292e

--- themes/default/templates/pfcclient.js.tpl.php
+++ themes/default/templates/pfcclient.js.tpl.php
@@ -678,7 +678,12 @@ pfcClient.prototype = {
         li.appendChild(img);
       }
       
-      li.appendChild(document.createTextNode(nicks[i]));      
+
+      // nobr is not xhtml valid but it's a workeround 
+      // for IE which doesn't support 'white-space: pre' css rule
+      var nobr = document.createElement('nobr');
+      nobr.appendChild(document.createTextNode(nicks[i]));
+      li.appendChild(nobr);
       li.style.borderBottom = '1px solid #AAA';
       
       ul.appendChild(li);
