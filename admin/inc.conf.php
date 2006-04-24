f3f478e893dee0c992f1a0cd40112c1f5e200f1e
Nemako <nemako@gmail.com>
2006-04-24 15:33:52 +0000

    Add a session to change dynamically the language.
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/trunk@468 2772adf2-ac07-0410-9d30-e29d8120292e

--- admin/inc.conf.php
+++ admin/inc.conf.php
@@ -1,6 +1,4 @@
 <?php
-//$lang = "en_US";
-$lang = "fr_FR";
-//$lang = isset($_GET["lang"]) ? $_GET["lang"] : (isset($_SESSION["lang"]) ? $_SESSION["lang"] : "en_US" ); //$_SESSION["lang"] = $lang;
-
+session_start();
+$lang = isset($_GET["lang"]) ? $_GET["lang"] : (isset($_SESSION["lang"]) ? $_SESSION["lang"] : "en_US" ); $_SESSION["lang"] = $lang;
 ?>
