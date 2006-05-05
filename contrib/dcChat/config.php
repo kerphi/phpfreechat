4b6f29e1442eeeab998deb28b65c197dcd5b31e1
kerphi <stephane.gully@gmail.com>
2006-05-05 15:46:21 +0000

    work on absolute urls
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/branches/0.x@486 2772adf2-ac07-0410-9d30-e29d8120292e

--- contrib/dcChat/config.php
+++ contrib/dcChat/config.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once dirname(__FILE__)."/phpfreechat/src/phpfreechattools.class.php";
+require_once dirname(__FILE__)."/phpfreechat/src/pfctools.php";
 require_once dirname(__FILE__)."/phpfreechat/src/phpfreechat.class.php";
 
 function &getChat($room)
@@ -9,12 +9,23 @@ function &getChat($room)
   if (!isset($chat))
   {
     $params = array();
+    $params["serverid"]      = md5(__FILE__);
     $params["title"]         = "";
     $params["channel"]       = $room;
-    $params["connect_at_startup"] = false;
-    $params["start_minimized"]    = true;
-    $params["server_script_url"]  = "ecrire/tools/dcchat/server_script.php?room=".$room;
-    $params["smileyurl"]          = "ecrire/tools/dcchat/phpfreechat/smileys";
+    //    $params["connect_at_startup"] = false;
+    //    $params["start_minimized"]    = true;
+
+    //echo "<pre>"; print_r($GLOBALS['news']);    echo "</pre>"; 
+    $url = $GLOBALS['theme_uri']."ecrire/tools/dcchat";
+    echo $url;
+    $params["server_script_url"]  = $url."/server_script.php?room=".$room;
+    // setup urls
+    $params["data_public_url"]   = $url."/phpfreechat/data/public";
+    //$params["client_script_url"] = "./demo21_with_hardcoded_urls.php";
+    $params["themeurl"]          = $url."/phpfreechat/themes";
+    $params["themeurl_default"]  = $url."/phpfreechat/themes";
+
+//    $params["smileyurl"]          = "ecrire/tools/dcchat/phpfreechat/smileys";
 //    $params["debug"]             = true;
     $chat = new phpFreeChat($params);
   }
