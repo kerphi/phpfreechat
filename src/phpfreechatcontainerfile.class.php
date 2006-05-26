136ed54c3b773e93a2bb3a654f611b9eaf450565
kerphi <stephane.gully@gmail.com>
2006-05-26 14:32:01 +0000

    Change the directory and file creation rights to 0700 (thanks to Thomas Lußnig for the notice)
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/branches/0.x@507 2772adf2-ac07-0410-9d30-e29d8120292e

--- src/phpfreechatcontainerfile.class.php
+++ src/phpfreechatcontainerfile.class.php
@@ -78,7 +78,7 @@ class phpFreeChatContainerFile extends phpFreeChatContainer
     if ($ok && !file_exists($c->container_cfg_data_file))
     {
       @touch($c->container_cfg_data_file);
-      @chmod($c->container_cfg_data_file, 0777);
+      @chmod($c->container_cfg_data_file, 0700);
     }
     if ($ok && !file_exists($c->container_cfg_data_file))
     {
@@ -113,7 +113,7 @@ class phpFreeChatContainerFile extends phpFreeChatContainer
     if ($ok && !file_exists($c->container_cfg_index_file))
     {
       @touch($c->container_cfg_index_file);      
-      @chmod($c->container_cfg_index_file, 0777);
+      @chmod($c->container_cfg_index_file, 0700);
     }
     if ($ok && !file_exists($c->container_cfg_index_file))
     {
@@ -168,7 +168,7 @@ class phpFreeChatContainerFile extends phpFreeChatContainer
     $my_filename = $c->container_cfg_nickname_dir."/".$this->_encode($c->nick);
     if (file_exists($my_filename)) $there = true;
     touch($my_filename);
-    @chmod($my_filename, 0777); 
+    @chmod($my_filename, 0700); 
     
     return $there;
   }
