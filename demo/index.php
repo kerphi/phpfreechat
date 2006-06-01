219bde5388740df1e38ee5784c99c3ae7e1005b4
kerphi <stephane.gully@gmail.com>
2006-06-01 08:41:44 +0000

    New Bulgarian bg_BG translation (thanks to Marko Nikolov)
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/trunk@541 2772adf2-ac07-0410-9d30-e29d8120292e

--- demo/index.php
+++ demo/index.php
@@ -109,6 +109,7 @@
       <li><a href="demo40_in_turkish.php">demo40 - the turkish translation of the chat</a></li>
       <li><a href="demo41_in_greek.php">demo41 - the greek translation of the chat</a></li>
       <li><a href="demo42_in_chinese_from_taiwan.php">demo42 - the Chinese from taiwan (traditional Chinese) translation of the chat</a></li>
+      <li><a href="demo45_in_bulgarian.php">demo45 - the Bulgarian translation of the chat</a></li>
     </ul>
 
 </div>
---
+++ i18n/bg_BG/main.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * i18n/bg_BG/main.php
+ *
+ * Copyright © 2006 Stephane Gully <stephane.gully@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details. 
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+/**
+ * Bulgarian translation of the messages (utf8 encoded!)
+ *
+ * @author Marko Nikolov <marko.nikolov@gmail.com>
+ */
+
+// line 45 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["My Chat"] = "Моят чат";
+
+// line 201 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s not found, %s library can't be found."] = "%s не е намерена, %s библиотека не е намерена.";
+
+// line 355 in phpfreechat.class.php
+$GLOBALS["i18n"]["Please enter your nickname"] = "Въведете вашия ник";
+
+// line 565 in phpfreechat.class.php
+$GLOBALS["i18n"]["Text cannot be empty"] = "Текстът не може да е празен";
+
+// line 392 in phpfreechat.class.php
+$GLOBALS["i18n"]["%s changes his nickname to %s"] = "%s си смени ника на %s";
+
+// line 398 in phpfreechat.class.php
+$GLOBALS["i18n"]["%s is connected"] = "%s влезе";
+
+// line 452 in phpfreechat.class.php
+$GLOBALS["i18n"]["%s quit"] = "%s излезе";
+
+// line 468 in phpfreechat.class.php
+$GLOBALS["i18n"]["%s disconnected (timeout)"] = "%s излезе (timeout)";
+
+// line 262 in phpfreechat.class.php
+$GLOBALS["i18n"]["Unknown command [%s]"] = "Непозната команда [%s]";
+
+// line 149 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s doesn't exist: %s"] = "%s не съществува: %s";
+
+// line 180 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["You need %s"] = "Трябва ви %s";
+
+// line 241 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s doesn't exist, %s library can't be found"] = "%s не съществува, %s библиотека не е намерена";
+
+// line 280 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s doesn't exist"] = "%s не съществува";
+
+// line 433 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s directory must be specified"] = "%s директория трябва да бъде посочена";
+
+// line 439 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s must be a directory"] = "%s трябва да е директория";
+
+// line 446 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s can't be created"] = "%s не може да се създаде";
+
+// line 451 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s is not writeable"] = "%s не може да се записва";
+
+// line 496 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s is not readable"] = "%s не може да се прочете";
+
+// line 469 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s is not a file"] = "%s не е файл";
+
+// line 491 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s is not a directory"] = "%s не е директория";
+
+// line 23 in chat.html.tpl.php
+$GLOBALS["i18n"]["PHP FREE CHAT [powered by phpFreeChat-%s]"] = "PHP FREE CHAT [powered by phpFreeChat-%s]";
+
+// line 296 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Hide nickname marker"] = "Скрий цветовете на никовете ";
+
+// line 304 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Show nickname marker"] = "Покажи цветовете на никовете";
+
+// line 389 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Disconnect"] = "Разкачи";
+
+// line 395 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Connect"] = "Свържи";
+
+// line 427 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Magnify"] = "Увеличи ";
+
+// line 434 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Cut down"] = "Смали";
+
+// line 345 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Hide dates and hours"] = "Скрий датата и часа";
+
+// line 353 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Show dates and hours"] = "Покажи датата и часа";
+
+// line 21 in chat.html.tpl.php
+$GLOBALS["i18n"]["Enter your message here"] = "Въведете тук вашето съобщение";
+
+// line 24 in chat.html.tpl.php
+$GLOBALS["i18n"]["Enter your nickname here"] = "Въведете тук вашия ник";
+
+// line 93 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["Error: undefined or obsolete parameter '%s', please correct or remove this parameter"] = "Грешка: неопределен или ненужен параметър '%s', моля поправете или премахнете този параметър";
+
+// line 86 in pfcclient.js.tpl.php
+$GLOBALS["i18n"]["Hide smiley box"] = "Скрий емотиконите";
+
+// line 87 in pfcclient.js.tpl.php
+$GLOBALS["i18n"]["Show smiley box"] = "Покажи емотиконите";
+
+// line 88 in pfcclient.js.tpl.php
+$GLOBALS["i18n"]["Hide online users box"] = "Скрий списъка с потребители";
+
+// line 89 in pfcclient.js.tpl.php
+$GLOBALS["i18n"]["Show online users box"] = "Покажи списъка с потребители";
+
+// line 33 in chat.html.tpl.php
+$GLOBALS["i18n"]["Bold"] = "Удебелен";
+
+// line 34 in chat.html.tpl.php
+$GLOBALS["i18n"]["Italics"] = "Наклонен";
+
+// line 35 in chat.html.tpl.php
+$GLOBALS["i18n"]["Underline"] = "Подчертан";
+
+// line 36 in chat.html.tpl.php
+$GLOBALS["i18n"]["Delete"] = "Изтрий";
+
+// line 37 in chat.html.tpl.php
+$GLOBALS["i18n"]["Pre"] = "Pre";
+
+// line 38 in chat.html.tpl.php
+$GLOBALS["i18n"]["Mail"] = "Поща";
+
+// line 39 in chat.html.tpl.php
+$GLOBALS["i18n"]["Color"] = "Цвят";
+
+// line 48 in phpfreechattemplate.class.php
+$GLOBALS["i18n"]["%s template could not be found"] = "%s шаблон не е намерен";
+
+// line 512 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["Error: '%s' could not be found, please check your themepath '%s' and your theme '%s' are correct"] = "Грешка: '%s' не е намерен, моля проверете точният път '%s' и вашата тема '%s' дали са правилни";
+
+// line 75 in pfccommand.class.php
+$GLOBALS["i18n"]["%s must be implemented"] = "%s трябва да бъде въведено";
+
+// line 343 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["'%s' parameter is mandatory by default use '%s' value"] = "'%s' параметър е задължителен, по подразбиране ползва '%s' стойност";
+
+// line 378 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["'%s' parameter must be a positive number"] = "'%s' парамерър трябва да е с положителна стойност";
+
+// line 386 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["'%s' parameter is not valid. Available values are : '%s'"] = "'%s' параметър не е валиден. Възможните стойности са: '%s'";
+?>
