69b188f6ca2e0ef284b9982fec91c7cbb92f4a0e
kerphi <stephane.gully@gmail.com>
2006-06-05 14:10:42 +0000

    XHTML fixes
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/trunk@547 2772adf2-ac07-0410-9d30-e29d8120292e

--- index.php
+++ index.php
@@ -7,17 +7,16 @@ $params["serverid"] = md5(__FILE__); // calculate a unique id for this chat
 $chat = new phpFreeChat( $params );
 
 ?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html>
  <head>
-  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
   <title>phpFreeChat Sources Index</title>
-  <link rel="stylesheet" title="classic" type="text/css" href="style/generic.css">
-  <link rel="stylesheet" title="classic" type="text/css" href="style/header.css">
-  <link rel="stylesheet" title="classic" type="text/css" href="style/footer.css">
-  <link rel="stylesheet" title="classic" type="text/css" href="style/menu.css">
-  <link rel="stylesheet" title="classic" type="text/css" href="style/content.css">  
+  <link rel="stylesheet" title="classic" type="text/css" href="style/generic.css" />
+  <link rel="stylesheet" title="classic" type="text/css" href="style/header.css" />
+  <link rel="stylesheet" title="classic" type="text/css" href="style/footer.css" />
+  <link rel="stylesheet" title="classic" type="text/css" href="style/menu.css" />
+  <link rel="stylesheet" title="classic" type="text/css" href="style/content.css" />  
   <?php $chat->printJavascript(); ?>
   <?php $chat->printStyle(); ?>  
  </head>
@@ -25,7 +24,7 @@ $chat = new phpFreeChat( $params );
 
 <div class="header">
       <h1>phpFreeChat - Sources Index</h1>
-      <img alt="logo bulle" src="style/bulle.png" class="logo2">
+      <img alt="logo bulle" src="style/bulle.png" class="logo2" />
 </div>
 
 <div class="menu">
@@ -60,7 +59,7 @@ $chat = new phpFreeChat( $params );
         </li>
       </ul>
       <p class="partner">
-        <a href="http://www.phpfreechat.net"><img alt="logo big" src="style/logo_88x31.gif"></a>
+        <a href="http://www.phpfreechat.net"><img alt="logo big" src="style/logo_88x31.gif" /></a>
       </p>
 </div>
 
@@ -72,10 +71,10 @@ $chat = new phpFreeChat( $params );
 <div class="footer">
       <div class="valid">
         <a href="http://validator.w3.org/check?uri=referer">
-          <img alt="Valid XHTML 1.0!" src="style/valid-xhtml.png">
+          <img alt="Valid XHTML 1.0!" src="style/valid-xhtml.png" />
         </a>
         <a href="http://jigsaw.w3.org/css-validator/check/referer">
-          <img alt="Valid CSS!" src="style/valid-css.png">
+          <img alt="Valid CSS!" src="style/valid-css.png" />
         </a>
       </div>
       <p>©2006 phpFreeChat</p>
