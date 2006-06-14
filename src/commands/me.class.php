5202ee45f47a634426c1b9fe9c750732bf594295
kerphi <stephane.gully@gmail.com>
2006-06-14 16:38:26 +0000

    Bug fix: the /me command was broken (thanks to Eury)
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/trunk@579 2772adf2-ac07-0410-9d30-e29d8120292e

--- src/commands/me.class.php
+++ src/commands/me.class.php
@@ -4,14 +4,15 @@ require_once(dirname(__FILE__)."/../pfccommand.class.php");
 
 class pfcCommand_me extends pfcCommand
 {
-  function run(&$xml_reponse, $clientid, $msg)
+  function run(&$xml_reponse, $clientid, $param, $sender, $recipient, $recipientid)
   {
     $c =& $this->c;
     $u =& $this->u;
 
     $container =& $c->getContainerInstance();
-    $msg = phpFreeChat::PreFilterMsg($msg);
-    $container->writeMsg("*me*", $u->nick." ".$msg);
+    $msg = phpFreeChat::PreFilterMsg($param);
+    $container->write($recipient, "*me*", $this->name, $u->nick." ".$msg);
+    
     if ($c->debug) pxlog("/me ".$msg, "chat", $c->getId());
   }
 }
