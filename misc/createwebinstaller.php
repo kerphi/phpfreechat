4bae0d4d86eb8cc6c3d0061733b5061725e0c308
kerphi <stephane.gully@gmail.com>
2006-09-26 19:07:34 +0000

    [en] The setup generator is now integrated to the command line process (many thanks to awright and his great webinstall project : http://sourceforge.net/projects/webinstall) [30min] [fr] Le generateur de setup est maintenant integré directement dans le processus de generation automatique de release en ligne de commande (merci beaucoup à awright et a son superbe webinstall : http://sourceforge.net/projects/webinstall) [30min]
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/trunk@796 2772adf2-ac07-0410-9d30-e29d8120292e

--- misc/createwebinstaller.php
+++ misc/createwebinstaller.php
@@ -4,15 +4,16 @@ $version = isset($_SERVER["argv"][1]) ? $_SERVER["argv"][1] : file_get_contents(
 $archivename = 'phpfreechat-'.$version.'-setup.php';
 $pfcpath = dirname(__FILE__).'/phpfreechat-'.$version;
 if (!file_exists($pfcpath)) die("Dont find the directory $pfcpath");
-$phpinstaller_path = realpath(dirname(__FILE__).'/../contrib/phpinstaller');
+$phpinstaller_path = realpath(dirname(__FILE__).'/../contrib/installer.beta-5.1');
 include($phpinstaller_path.'/engine.inc.php');
 $phpi = new phpInstaller();
-$phpi->dataDir($phpinstaller_path.'/engene_data');
+$phpi->dataDir($phpinstaller_path.'/engine_data');
 $phpi->appName = 'phpFreeChat';
 $phpi->appVersion = $version;
 $phpi->addMetaFile('ss',$phpinstaller_path.'/createinstaller/data/installer.css','text/css')
   or die('Can not find stylesheet');
 $phpi->ignore[] = '.svn';
+$phpi->addPage('Pre-Install Check',file_get_contents($phpinstaller_path.'/createinstaller/data/precheck.inc'));
 $phpi->addInstallerPages();
 $phpi->addPath($pfcpath);
 $phpi->generate($archivename);
