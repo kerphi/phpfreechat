c1fad08c49de8373dbf78ebac6cd369e073b305c
kerphi <stephane.gully@gmail.com>
2007-01-06 12:45:49 +0000

    [en] Add the Korean translation (ko_KR) (thanks to Kim Taeho - http://zxcv.mireene.com/zb5/) [0h20] [fr] Ajoute la tranduction Coréenne (ko_KR) (merci à Kim Taeho - http://zxcv.mireene.com/zb5/) [0h20]
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/trunk@926 2772adf2-ac07-0410-9d30-e29d8120292e

---
+++ i18n/ko_KR/main.php
@@ -0,0 +1,309 @@
+<?php
+/**
+ * i18n/ko_KR/main.php
+ *
+ * Copyright © 2006 Stephane Gully <stephane.gully@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details. 
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+/**
+ * Korean translation of the messages..
+ *
+ * @author Kim Taeho (taehos@gmail.com)
+ */
+
+// line 45 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["My Chat"] = "내 채팅방";
+
+// line 201 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s not found, %s library can't be found."] = "%s 를 찾을 수 없습니다, %s 라이브러리가 로드되지 않았습니다.";
+
+// line 355 in phpfreechat.class.php
+$GLOBALS["i18n"]["Please enter your nickname"] = "사용할 닉네임을 입력하세요";
+
+// line 565 in phpfreechat.class.php
+$GLOBALS["i18n"]["Text cannot be empty"] = "빈칸을 입력하실 순 없습니다.";
+
+// line 392 in phpfreechat.class.php
+$GLOBALS["i18n"]["%s changes his nickname to %s"] = "%s 님께서 %s로 닉을 바꾸셨습니다.";
+
+// line 398 in phpfreechat.class.php
+$GLOBALS["i18n"]["%s is connected"] = "%s 님 반갑습니다. 즐거운 시간 되세요.";
+
+// line 452 in phpfreechat.class.php
+$GLOBALS["i18n"]["%s quit"] = "%s 님께서 나가셨습니다.";
+
+// line 468 in phpfreechat.class.php
+$GLOBALS["i18n"]["%s disconnected (timeout)"] = "%s 님의 연결이 끊겼습니다 (타임아웃)";
+
+// line 262 in phpfreechat.class.php
+$GLOBALS["i18n"]["Unknown command [%s]"] = "존재하지 않는 명령입니다. [%s]";
+
+// line 149 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s doesn't exist: %s"] = "%s 는 존재하지 않습니다 : %s";
+
+// line 180 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["You need %s"] = "%s 가 필요합니다.";
+
+// line 241 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s doesn't exist, %s library can't be found"] = "%s 를 찾을 수 없습니다, %s 라이브러리가 로드되지 않았습니다.";
+
+// line 280 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s doesn't exist"] = "%s 는 존재하지 않습니다.";
+
+// line 433 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s directory must be specified"] = "%s 디렉토리는 선택되어야만 합니다.";
+
+// line 439 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s must be a directory"] = "%s 는 디렉토리 이어야만 합니다.";
+
+// line 446 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s can't be created"] = "%s 는 생성될 수 없습니다.";
+
+// line 451 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s is not writeable"] = "%s 에 기록할 수 없습니다.";
+
+// line 496 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s is not readable"] = "%s 를 읽을수 없습니다.";
+
+// line 469 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s is not a file"] = "%s 는 파일이 아닙니다.";
+
+// line 491 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["%s is not a directory"] = "%s 는 디렉토리가 아닙니다.";
+
+// line 23 in chat.html.tpl.php
+$GLOBALS["i18n"]["PHP FREE CHAT [powered by phpFreeChat-%s]"] = "PHP FREE CHAT [powered by phpFreeChat-%s]";
+
+// line 296 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Hide nickname marker"] = "닉네임에 색상 사용하지 않기";
+
+// line 304 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Show nickname marker"] = "닉네임에 색상 사용하기";
+
+// line 389 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Disconnect"] = "연결 해제";
+
+// line 395 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Connect"] = "연결";
+
+// line 427 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Magnify"] = "확대";
+
+// line 434 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Cut down"] = "자르기";
+
+// line 345 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Hide dates and hours"] = "날짜와 시간 숨기기";
+
+// line 353 in javascript1.js.tpl.php
+$GLOBALS["i18n"]["Show dates and hours"] = "날짜와 시간 표시하기";
+
+// line 21 in chat.html.tpl.php
+$GLOBALS["i18n"]["Enter your message here"] = "여기에 대화를 입력하세요";
+
+// line 24 in chat.html.tpl.php
+$GLOBALS["i18n"]["Enter your nickname here"] = "여기에 별명을 입력하세요";
+
+// line 93 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["Error: undefined or obsolete parameter '%s', please correct or remove this parameter"] = "오류 : '%s'는 정의되지 않거나 사용할 수 없는 피라메터 입니다, 이 피라메터를 없애거나 수정하세요..";
+
+// line 86 in pfcclient.js.tpl.php
+$GLOBALS["i18n"]["Hide smiley box"] = "이모티콘 숨기기";
+
+// line 87 in pfcclient.js.tpl.php
+$GLOBALS["i18n"]["Show smiley box"] = "이모티콘 보이기";
+
+// line 88 in pfcclient.js.tpl.php
+$GLOBALS["i18n"]["Hide online users box"] = "현재 접속자 숨기기";
+
+// line 89 in pfcclient.js.tpl.php
+$GLOBALS["i18n"]["Show online users box"] = "현재 접속자 보이기";
+
+// line 33 in chat.html.tpl.php
+$GLOBALS["i18n"]["Bold"] = "굵게";
+
+// line 34 in chat.html.tpl.php
+$GLOBALS["i18n"]["Italics"] = "이탤릭";
+
+// line 35 in chat.html.tpl.php
+$GLOBALS["i18n"]["Underline"] = "밑줄";
+
+// line 36 in chat.html.tpl.php
+$GLOBALS["i18n"]["Delete"] = "취소선";
+
+// line 37 in chat.html.tpl.php
+$GLOBALS["i18n"]["Pre"] = "Pre";
+
+// line 38 in chat.html.tpl.php
+$GLOBALS["i18n"]["Mail"] = "메일주소";
+
+// line 39 in chat.html.tpl.php
+$GLOBALS["i18n"]["Color"] = "색상";
+
+// line 48 in phpfreechattemplate.class.php
+$GLOBALS["i18n"]["%s template could not be found"] = "%s 템플릿을 찾을 수 없습니다.";
+
+// line 512 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["Error: '%s' could not be found, please check your themepath '%s' and your theme '%s' are correct"] = "Error: '%s' 를 찾을 수 없습니다. 테마의 위치인 '%s' 를 찾아보시고 지정한 테마가 '%s' 가 맞는지 확인하세요";
+
+// line 75 in pfccommand.class.php
+$GLOBALS["i18n"]["%s must be implemented"] = "%s 는 권한이 있어야만 합니다.";
+
+
+// line 343 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["'%s' parameter is mandatory by default use '%s' value"] = "'%s' 피라메터는 필수적으로 요구됩니다. '%s' 값을 사용하기 위해...";
+
+// line 378 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["'%s' parameter must be a positive number"] = "'%s' 피라메터는 양수 이어야 합니다.";
+
+// line 386 in phpfreechatconfig.class.php
+$GLOBALS["i18n"]["'%s' parameter is not valid. Available values are : '%s'"] = "'%s' 피라메터는 유효하지 않습니다. 사용가능한 목록 : '%s'";
+
+// line 185 in pfcglobalconfig.class.php
+$GLOBALS["i18n"]["My room"] = "내 방";
+
+// line 109 in pfcclient.js.tpl.php
+$GLOBALS["i18n"]["Private message"] = "개인 메세지";
+
+// line 110 in pfcclient.js.tpl.php
+$GLOBALS["i18n"]["Close this tab"] = "이 탭 닫기";
+
+// line 225 in pfcgui.js.tpl.php
+$GLOBALS["i18n"]["Do you really want to leave this room ?"] = "이 채널을 떠나길 원하십니까?";
+
+// line 19 in unban.class.php
+$GLOBALS["i18n"]["Missing parameter"] = "피라미터를 찾을수 없습니다.";
+
+// line 38 in ban.class.php
+$GLOBALS["i18n"]["banished from %s by %s"] = "%s 로 부터 강퇴되었습니다. %s에 의해..";
+
+// line 23 in banlist.class.php
+$GLOBALS["i18n"]["The banished user's id list is:"] = "접근 금지자 목록:";
+
+// line 32 in banlist.class.php
+$GLOBALS["i18n"]["Empty"] = "비어있음";
+
+// line 34 in banlist.class.php
+$GLOBALS["i18n"]["'/unban {id}' will unban the user identified by {id}"] = "'/unban {id}' 명령은 {id}로 식별되는 사용자를 추방한걸 취소합니다.";
+
+// line 35 in banlist.class.php
+$GLOBALS["i18n"]["'/unban all'  will unban all the users on this channel"] = "'/unban all' 명령은 이 채널에 있는 모든 사용자를 추방한걸 취소합니다.";
+
+// line 24 in update.class.php
+$GLOBALS["i18n"]["%s quit (timeout)"] = "%s 님께서 나갔습니다. (타임아웃)";
+
+// line 46 in join.class.php
+$GLOBALS["i18n"]["%s joins %s"] = "%s 님께서 %s 에 들어오셨습니다.";
+
+// line 31 in kick.class.php
+$GLOBALS["i18n"]["kicked from %s by %s"] = "%s 로 부터 퇴장 되었습니다. %s에 의해";
+
+// line 38 in send.class.php
+$GLOBALS["i18n"]["Can't send the message, %s is offline"] = "메시지를 보낼 수 없습니다, %s 님은 오프라인 상태 입니다.";
+
+// line 27 in unban.class.php
+$GLOBALS["i18n"]["Nobody has been unbanished"] = "아무도 추방 취소되지 않았습니다.";
+
+// line 42 in unban.class.php
+$GLOBALS["i18n"]["%s has been unbanished"] = "%s 님의 추방을 취소했습니다.";
+
+// line 49 in unban.class.php
+$GLOBALS["i18n"]["%s users have been unbanished"] = "%s 님은 추방이 취소 되어 있습니다.";
+
+// line 47 in auth.class.php
+$GLOBALS["i18n"]["You are not allowed to run '%s' command"] = "'%s'명령을 실행할 권한이 없습니다.";
+
+// line 67 in auth.class.php
+$GLOBALS["i18n"]["Can't join %s because you are banished"] = "%s 에 참여할수 없습니다(강퇴)";
+
+// line 79 in auth.class.php
+$GLOBALS["i18n"]["You are not allowed to change your nickname"] = "닉을 바꿀 권한이 없습니다.";
+
+// line 76 in auth.class.php
+$GLOBALS["i18n"]["Can't join %s because the channels list is restricted"] = "%s 에 참여할 수 없습니다(제한된 채널)";
+
+// line 56 in noflood.class.php
+$GLOBALS["i18n"]["Please don't post so many message, flood is not tolerated"] = "도배 하지 마세요...";
+
+// line 169 in pfcglobalconfig.class.php
+$GLOBALS["i18n"]["Error: '%s' is a private parameter, you are not allowed to change it"] = "Error: '%s' 는 개인 피라메터 입니다. 수정하실 수 없습니다.";
+
+// line 253 in pfcglobalconfig.class.php
+$GLOBALS["i18n"]["'%s' parameter must be an array"] = "'%s' 피라메터는 어레이로 처리되어야 합니다.";
+
+// line 265 in pfcglobalconfig.class.php
+$GLOBALS["i18n"]["'%s' parameter must be a boolean"] = "'%s' 피라메터는 부울값으로 있어야 합니다.";
+
+// line 271 in pfcglobalconfig.class.php
+$GLOBALS["i18n"]["'%s' parameter must be a charatere string"] = "'%s' 피라메터는 글자이어야 합니다.";
+
+// line 395 in pfcglobalconfig.class.php
+$GLOBALS["i18n"]["'%s' must be writable"] = "'%s' 에 쓸 수 있어야 합니다.";
+
+// line 425 in pfcglobalconfig.class.php
+$GLOBALS["i18n"]["'%s' directory doesn't exist"] = "'%s' 디렉토리가 존재하지 않습니다.";
+
+// line 544 in pfcglobalconfig.class.php
+$GLOBALS["i18n"]["Please correct these errors"] = "위 오류를 수정해 주시기 바랍니다.";
+
+// line 21 in pfcinfo.class.php
+$GLOBALS["i18n"]["Error: the cached config file doesn't exists"] = "오류 : 저장된 설정이 존재하지 않습니다.";
+
+// line 190 in phpfreechat.class.php
+$GLOBALS["i18n"]["Error: the chat cannot be loaded! two possibilities: your browser doesn't support javascript or you didn't setup correctly the server directories rights - don't hesitate to ask some help on the forum"] = "오류 : 채팅이 로드될 수 없습니다! 두가지 가능성: 브라우저에서 자바스크립트를 지원하지 않거나, 서버의 폴더 권한을 잘 설정하지 못했을 수 있습니다. - 포럼의 누군가에게 묻는것에 대해 망설이지 마십시오.";
+
+// line 31 in help.class.php
+$GLOBALS["i18n"]["Here is the command list:"] = "명령어 목록:";
+
+// line 63 in identify.class.php
+$GLOBALS["i18n"]["Succesfully identified"] = "성공적으로 인증됨";
+
+// line 68 in identify.class.php
+$GLOBALS["i18n"]["Identification failure"] = "인증 실패";
+
+// line 25 in send.class.php
+$GLOBALS["i18n"]["Your must be connected to send a message"] = "메세지를 보내려면 연결해야 합니다.";
+
+// line 87 in chat.js.tpl.php
+$GLOBALS["i18n"]["Click here to send your message"] = "여기를 눌러 메세지를 보내세요";
+
+// line 80 in chat.js.tpl.php
+$GLOBALS["i18n"]["Enter the text to format"] = "형식화 할 글자를 넣으세요..";
+
+// line 81 in chat.js.tpl.php
+$GLOBALS["i18n"]["Configuration has been rehashed"] = "재설정";
+
+// line 82 in chat.js.tpl.php
+$GLOBALS["i18n"]["A problem occurs during rehash"] = "재설정중 문제가 생겼습니다.";
+
+// line 83 in chat.js.tpl.php
+$GLOBALS["i18n"]["Choosen nickname is allready used"] = "이미 사용중인 별명입니다.";
+
+// line 84 in chat.js.tpl.php
+$GLOBALS["i18n"]["phpfreechat current version is %s"] = "phpfreechat 의 현재 버전은 %s 입니다.";
+
+// line 85 in chat.js.tpl.php
+$GLOBALS["i18n"]["Maximum number of joined channels has been reached"] = "최대 방 인원에 도달하였습니다.";
+
+// line 86 in chat.js.tpl.php
+$GLOBALS["i18n"]["Maximum number of private chat has been reached"] = "최대 개인채팅수에 도달하였습니다.";
+
+// line 88 in chat.js.tpl.php
+$GLOBALS["i18n"]["Send"] = "입력";
+
+?>
