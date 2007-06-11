1c0d4c852be6861c21c2b99a2168e70f304641b7
kerphi <stephane.gully@gmail.com>
2007-06-11 08:19:13 +0000

    Fix a problem with phpbb3 integration (thanks to Larry) http://www.phpfreechat.net/forum/viewtopic.php?id=1576
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/trunk@1034 2772adf2-ac07-0410-9d30-e29d8120292e

--- lib/utf8/utf8_strlen.php
+++ lib/utf8/utf8_strlen.php
@@ -9,16 +9,18 @@
  * @see strlen()
  * @author    Martin Kutschker <martin.t.kutschker@blackbox.net>
  */
-function utf8_strlen($str)    {
-  $n=0;
-  for($i=0; isset($str{$i}) && strlen($str{$i})>0; $i++)    {
-    $c = ord($str{$i});
-    if (!($c & 0x80))    // single-byte (0xxxxxx)
-      $n++;
-    elseif (($c & 0xC0) == 0xC0)    // multi-byte starting byte (11xxxxxx)
-      $n++;
+if (!function_exists('utf8_strlen')) {
+  function utf8_strlen($str)    {
+    $n=0;
+    for($i=0; isset($str{$i}) && strlen($str{$i})>0; $i++)    {
+      $c = ord($str{$i});
+      if (!($c & 0x80))    // single-byte (0xxxxxx)
+        $n++;
+      elseif (($c & 0xC0) == 0xC0)    // multi-byte starting byte (11xxxxxx)
+        $n++;
+    }
+    return $n;
   }
-  return $n;
 }
 
 ?>
--- lib/utf8/utf8_substr.php
+++ lib/utf8/utf8_substr.php
@@ -14,28 +14,30 @@ require_once dirname(__FILE__)."/utf8_char2byte_pos.php";
  * @see substr()
  * @author    Martin Kutschker <martin.t.kutschker@blackbox.net>
  */
-function utf8_substr($str,$start,$len=null)    {
-  if (!strcmp($len,'0'))    return '';
+if (!function_exists('utf8_substr')) {
+  function utf8_substr($str,$start,$len=null)    {
+    if (!strcmp($len,'0'))    return '';
   
-  $byte_start = @utf8_char2byte_pos($str,$start);
-  if ($byte_start === false)    {
-    if ($start > 0)    {
-      return false;    // $start outside string length
-    } else {
-      $start = 0;
+    $byte_start = @utf8_char2byte_pos($str,$start);
+    if ($byte_start === false)    {
+      if ($start > 0)    {
+        return false;    // $start outside string length
+      } else {
+        $start = 0;
+      }
     }
-  }
   
-  $str = substr($str,$byte_start);
+    $str = substr($str,$byte_start);
   
-  if ($len!=null)    {
-    $byte_end = @utf8_char2byte_pos($str,$len);
-    if ($byte_end === false)    // $len outside actual string length
-      return $len<0 ? '' : $str;    // When length is less than zero and exceeds, then we return blank string.
-    else
-      return substr($str,0,$byte_end);
+    if ($len!=null)    {
+      $byte_end = @utf8_char2byte_pos($str,$len);
+      if ($byte_end === false)    // $len outside actual string length
+        return $len<0 ? '' : $str;    // When length is less than zero and exceeds, then we return blank string.
+      else
+        return substr($str,0,$byte_end);
+    }
+    else    return $str;
   }
-  else    return $str;
 }
 
 ?>
