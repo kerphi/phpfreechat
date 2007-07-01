6fe516039bae41231e94a722607e0cbc2b61bf2b
kerphi <stephane.gully@gmail.com>
2007-07-01 15:17:56 +0000

    The login box was broken on KHTML browsers
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/trunk@1051 2772adf2-ac07-0410-9d30-e29d8120292e

--- data/public/js/pfcprompt.js
+++ data/public/js/pfcprompt.js
@@ -119,7 +119,7 @@ pfcPrompt.prototype = {
   {
     // _doSubmit is called when the user enters or cancels the box.
     var val = this.prompt_field.value;
-    this.box.focus();
+    if (is_ff) this.box.focus(); // test is_ff because it doesn't work on KHTML browser, the popup shows infinitly
     this.box.style.display   = 'none'; // clear out the dialog box
     this.bgbox.style.display = 'none'; // clear out the screen
     this.prompt_field.value  = ''; // clear out the text field
