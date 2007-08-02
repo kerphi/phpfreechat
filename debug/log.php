5a12d0cdce1ffee87f1a6e1ddd10e65f57e6413b
Gpinzone <gpinzone@gmail.com>
2007-08-02 18:59:18 +0000

    Updated pxlog function to use file_put_contents and honor file locking.
    git-svn-id: https://phpfreechat.svn.sourceforge.net/svnroot/phpfreechat/trunk@1078 2772adf2-ac07-0410-9d30-e29d8120292e

--- debug/log.php
+++ debug/log.php
@@ -4,11 +4,8 @@ function pxlog($data, $section = "", $id = "")
 {
   $msg = htmlspecialchars(stripslashes(var_export($data, true)));
   $filename = dirname(__FILE__)."/../data/private/debug".$section."_".$id.".log";
-  if (!file_exists($filename))
-    @touch($filename);
-  $fp = @fopen($filename, 'a');
-  @fwrite($fp, "[".$id."] ".date("Y/m/d H:i:s - ").$msg."\n");
-  @fclose($fp);
+
+  @file_put_contents($filename, "[".$id."] ".date("Y/m/d H:i:s - ").$msg."\n", FILE_APPEND | LOCK_EX);
 }
 
 ?>
