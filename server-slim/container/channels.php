9e8003f8039a5ec41659decfe4dca063fbd0ddf0
Stéphane Gully <stephane.gully@gmail.com>
2012-08-06 22:52:52 +0200

    slim: implements PUT /channels/:cid/users/:uid

---
+++ server-slim/container/channels.php
@@ -0,0 +1,59 @@
+<?php
+
+class Container_channels {
+  
+  static public function getChannelsDir() {
+    $datadir = __DIR__.'/../data';
+    $cdir = $datadir.'/channels';
+    return $cdir;
+  }
+  
+  static public function generateCid() {
+    $cdir = self::getChannelsDir();
+    do {
+      $cid = sha1(uniqid('', true));
+      $cpath = $cdir.'/'.$cid.'/';
+    } while (file_exists($cpath));
+    @mkdir($cpath, 0777, true);
+    @mkdir($cpath.'/users', 0777, true);
+    return $cid;
+  }
+  
+  
+  static public function getChannelMsgPath($cid, $mid) {
+    $cdir = self::getChannelsDir();
+    $mpath = $cdir.'/'.$cid.'/msg/'.$mid.'.json';
+    return $mpath;
+  }
+  
+  static public function getChannelUserPath($cid, $uid) {
+    $cdir = self::getChannelsDir();
+    $cupath = $cdir.'/'.$cid.'/users/'.$uid;
+    return $cupath;
+  }
+  
+  static public function getChannelUsers($cid, $withudata = false) {
+    $cdir = self::getChannelsDir();
+    $cupath = $cdir.'/'.$cid.'/users/';  
+    $subscribers = array();
+    foreach(scandir($cupath) as $value) {
+      if($value === '.' || $value === '..') {continue;}
+      if ($withudata) {
+        $subscribers[$value] = Container_users::getUserData($value);
+      } else {
+        $subscribers[] = $value;
+      }
+    }
+    return $subscribers;
+  }
+  
+  /**
+   * Check if a user is on the given channel
+   */
+  static public function checkChannelUser($cid, $uid) {
+    $cdir = self::getChannelsDir();
+    $cupath = $cdir.'/'.$cid.'/users/'.$uid;  
+    return file_exists($cupath);
+  }  
+}
+
--- server-slim/routes/channels.php
+++ server-slim/routes/channels.php
@@ -1,6 +1,7 @@
 <?php
 
 include_once 'container/users.php';
+include_once 'container/channels.php';
 
 /**
  * Returns channel list
@@ -33,9 +34,45 @@ $app->get('/channels/:cid/users/', function ($cid) use ($app, $req, $res) {
  * Join a channel
  */
 $app->put('/channels/:cid/users/:uid', function ($cid, $uid) use ($app, $req, $res) {
-  $res->status(501);
-  $res['Content-Type'] = 'application/json; charset=utf-8';
-  $res->body('join a channel '.$cid);
+  
+  // check user acces
+  session_start();
+  if (!isset($_SESSION['userdata']) or !isset($_SESSION['userdata']['id'])) {
+    $res->status(401); // Need to authenticate
+    return;
+  }
+  if ($uid !== $_SESSION['userdata']['id']) {
+    $res->status(403); // Forbidden
+    return;
+  }
+
+  // check this user is online
+  if (!Container_users::checkUserExists($uid)) {
+    $res->status(400); // User is not connected
+    return;
+  }
+  
+  // todo remove this code when channel create/join will be implemented
+  $cdir = Container_channels::getChannelsDir();
+  $cpath = $cdir.'/'.$cid.'/';
+  @mkdir($cpath, 0777, true);
+  @mkdir($cpath.'/users', 0777, true);
+  
+  if (!Container_users::joinChannel($uid, $cid)) {
+    $res->status(200); // header('HTTP/1.1 200 User already subscribed');
+    $res['Content-Type'] = 'application/json; charset=utf-8';
+    $res->body(json_encode(Container_channels::getChannelUsers($cid, true)));
+    return;
+  } else {
+    // post a join message
+    $msg = Container_messages::postMsgToChannel($cid, $uid, null, 'join');
+    
+    $res->status(201); //    header('HTTP/1.1 201 User joined the channel');
+    $res['Content-Type'] = 'application/json; charset=utf-8';
+    $res->body(json_encode(Container_channels::getChannelUsers($cid, true)));
+    return;
+  }
+
 });
 
 /**
