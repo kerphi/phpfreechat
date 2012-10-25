30f3a94858a01c24d76ebe4b599bc6da86741730
Stéphane Gully <stephane.gully@gmail.com>
2012-10-25 23:00:40 +0200

    move /server/config route to /check.php and check that php version is compatible with phpfreechat

---
+++ check.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Script in charge of checking server configuration and returns a status
+ */
+
+$status = array();
+
+// check that server/data/ folder is writable
+if (!is_writable(dirname(__FILE__).'/data/')) {
+  $status[] = basename(dirname(dirname(__FILE__))).'/server/data/ folder is not writable by your Web server. Please adjust folder permissions to 777.';
+}
+
+// check that php version dependency is respected
+if (version_compare(PHP_VERSION, '5.3.0', '<')) {
+  $status[] = 'Your php version '.PHP_VERSION.' must be >= 5.3.0';
+}
+
+header("HTTP/1.1 200");
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($status);
--- server/index.php
+++ server/index.php
@@ -28,6 +28,5 @@ foreach ($GLOBALS['pfc_hooks'] as $hook_name => $hooks) {
 require 'routes/auth.php';
 require 'routes/channels.php';
 require 'routes/users.php';
-require 'routes/config.php';
 
 $app->run();
